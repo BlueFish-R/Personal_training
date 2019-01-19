b4330b4088cb339ec1120b69a47c5d8356bb61c0
RSY <yugewansui267@163.com>
2019-01-19 08:59:35 +0800

    简化代码“

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 """
-1、编写函数实现topk查找，输入为整数数组与k，如[12,3,9,10,89,46,2,109,-1],k为5，输入[109,89,46,12,10
+1、编写函数实现topk查找，输入为整数数组与k，如[12,3,9,10,89,46,2,109,-1],k为5，输出[109,89,46,12,10
 """
 
-def topk(k,list):
-    list = sorted(list)[::-1]
 
-    topk_list = list[-1:-1-k:-1]
-    print(topk_list)
+def topk(k, list):
+    list = sorted(list)[::-1][0:k]
+    print(list)
 
-topk(5,[12,3,9,10,89,46,2,109,-1])
+
+topk(5, [12, 3, 9, 10, 89, 46, 2, 109, -1])
 
 """
 2、使用一段代码实现对字符串的简单压缩，例如将‘abbccccddd’压缩为‘a1b2c4d3’，如果压缩后的字符串没有变短则返回原字符串
@@ -20,7 +20,7 @@ head = str1[0]
 num = 0
 temp = False
 
-for i in str1+"1":
+for i in str1 + "1":
     if i == head:
         num += 1
     elif i != head:
